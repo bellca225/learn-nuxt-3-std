3b23e25ac61f17e6acfaa2426461a49bf01f634f
bellca225 <nywoo0225@gmail.com>
2025-02-03 22:42:48 +0900

    eslint setting add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-02-03",
+  compatibilityDate: '2025-02-03',
   devtools: { enabled: true },
   typescript: {
     typeCheck: true,
