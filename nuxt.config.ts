df3b7e516c4d37064bcb8fa47a9fb0d523d5e9bf
bellca225 <nywoo0225@gmail.com>
2025-02-03 22:24:44 +0900

    nuxi로 명령어 변경, ts config 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  compatibilityDate: "2025-02-03",
   devtools: { enabled: true },
+  typescript: {
+    typeCheck: true,
+  },
 });
 module.exports = {
   telemetry: true,
